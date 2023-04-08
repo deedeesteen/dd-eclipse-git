74af3e695de59ee75afe67021dbaf258667cf011
Dhana Steen <30987447+deedeesteen@users.noreply.github.com>
2023-04-08 08:12:56 -0500

    Add files via upload

---
+++ Query.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Query extends Complaint{
+	private String QueryID;
+	
+	//Primary Constructor
+    public Query(String category) {
+        super(category);
+    }
+}
