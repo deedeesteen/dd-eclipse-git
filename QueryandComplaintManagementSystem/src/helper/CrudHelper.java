10db4ac933084f2daff18b63b5ff812b1c5bf546
Dhana Steen <30987447+deedeesteen@users.noreply.github.com>
2023-04-20 07:15:26 -0500

    Update CrudHelper.java

--- QueryandComplaintManagementSystem/src/helper/CrudHelper.java
+++ QueryandComplaintManagementSystem/src/helper/CrudHelper.java
@@ -1,8 +1,10 @@
 package helper;
+
 /*DEVELOPERS:
  * Dhana Steen, ID# 1803365
  * Sassania Hibbert, ID# 1901202
  */
+ 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,6 +13,7 @@ import java.sql.Statement;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import javax.swing.JOptionPane;
+import java.io.Serializable;
 
 import server.UserType;
 
@@ -42,9 +45,9 @@ public class CrudHelper {
 		dbConn = getDatabaseConnection();
 	}
 	
-	public void createUser(String id,String firstName, String lastName, UserType userType) {
+	public void createUser(String id, String firstName, String lastName, UserType userType) {
 		String insertSql = "INSERT INTO studentservices.User (id, firstName,lastName, userType)" 
-				+"VALUES('"+ id +"','" +firstName+"','"+lastName+"','"+userType+"');";
+				+"VALUES('"+id+ "', '" +firstName+ "', '" +lastName+ "', '" +userType+ "')";
 		
 		try {
 			stmt = dbConn.createStatement();
@@ -62,7 +65,7 @@ public class CrudHelper {
 	}
 	
 	public void getUser(String id) {
-		String selectSQL = "SELECT * FROM studentservices.User WHERE id ='"+id;
+		String selectSQL = "SELECT * FROM studentservices.User WHERE id = '" +id+ "'";
 		try {
 			stmt = dbConn.createStatement();
 			result = stmt.executeQuery(selectSQL);
@@ -70,12 +73,70 @@ public class CrudHelper {
 				 String  firstName = result.getString("firstName");
 				 String  lastName = result.getString("lastName");
 				 String  userType = result.getString("userType");
-				 System.out.println("Id: " + id +"\nName: "+firstName+" "+lastName+"\nUserType: "+userType);	 	 
+				 System.out.println("Id: " + id +"\nName: "+ firstName +" "+ lastName +"\nUserType: "+ userType);	 	 
 			}
 		}catch(SQLException e) {
 			System.err.println("SQL EXCEPTION:" +e.getMessage());
 			
 		}catch(Exception e) {
+			
+		}
+	}
+	
+	public void createService(String serviceID, String id, Date date_opened, Date date_resolved) {
+		String insertCSSql = "INSERT INTO studentservices.Service (serviceID, id, date_opened, date_resolved)" 
+				+"VALUES('" +serviceID+ "', '" +id+ "', '" +date_opened+ "', '" +date_resolved+ "')";
+		
+		try {
+			stmt = dbConn.createStatement();
+			int inserted = stmt.executeUpdate(insertCSSql);
+			if(inserted == 1) {
+				JOptionPane.showMessageDialog(null,"Record Inserted", 
+						"Record Insertion Status",JOptionPane.INFORMATION_MESSAGE);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public void getService(String serviceID, String id, Date date_opened, Date date_resolved) {
+		String selectServSQL = "SELECT * FROM studentservices.Service WHERE id = '" +id+ "'";
+		try {
+			stmt = dbConn.createStatement();
+			result = stmt.executeQuery(selectServSQL);
+			while(result.next()) {
+				 String  serviceID = result.getString("serviceID");
+				 String  id = result.getString("id");
+				 Date date_opened = result.getDate("date_opened");
+				 Date date_resolved = result.getDate("date_resolved");
+				 System.out.println("Service ID: " + serviceID + "\nId: " + id + "\nDate Opened: " + date_opened + "\nDate Resolved: "+ date_resolved);	 	 
+			}
+		}catch(SQLException e) {
+			System.err.println("SQL EXCEPTION:" +e.getMessage());
+			
+		}catch(Exception e) {
+			
+		}
+	}
+	
+	public void createComplaint(String ComplaintID, String category) {
+		String insertCompSql = "INSERT INTO studentservices.Complaint (ComplaintID, category)" 
+				+"VALUES('" +ComplaintID+ "', '" +category+ "')";
+		
+		try {
+			stmt = dbConn.createStatement();
+			int inserted = stmt.executeUpdate(insertCompSql);
+			if(inserted == 1) {
+				JOptionPane.showMessageDialog(null,"Record Inserted", 
+						"Record Insertion Status",JOptionPane.INFORMATION_MESSAGE);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
 			
 		}
 	}
